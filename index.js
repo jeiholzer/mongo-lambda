08e24f6f81773456764fa1449404b550c5a91498
Matthew Collier <collier.matthew@gmail.com>
2017-09-22 21:48:58 -0400

    Add core files.

---
+++ index.js
@@ -0,0 +1,37 @@
+const async = require('async');
+const bedrock = require('bedrock');
+const database = require('bedrock-mongodb');
+const uuid = require('uuid/v4');
+const AWS = require('aws-sdk');
+
+const lambda = new AWS.Lambda();
+
+bedrock.events.on('bedrock-mongodb.ready', callback => async.auto({
+  openCollections: callback => database.openCollections(['lambda'], callback)
+}, err => callback(err)));
+
+bedrock.events.on('bedrock.started', callback => {
+  const p1 = uuid();
+  const p2 = uuid();
+  const query = {p1};
+  async.auto({
+    insert: callback => database.collections.lambda.insert({p1, p2}, callback),
+    find: ['insert', (results, callback) => database.collections.lambda
+      .find(query).toArray((err, result) => {
+        console.log('LOCAL-FIND', JSON.stringify(result, null ,2));
+        callback();
+      })],
+    lambda: ['insert', (results, callback) => {
+      const params = {
+        FunctionName: 'mongoLambda',
+        Payload: JSON.stringify(query)
+      };
+      lambda.invoke(params, (err, result) => {
+        console.log('LAMBDA-FIND', JSON.stringify(result, null, 2));
+        callback();
+      });
+    }]
+  }, callback);
+});
+
+bedrock.start();
